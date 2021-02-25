e94775389d22db90475e27b0d963b0e2f0f2bfbb
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-02-25 08:50:28 +0100

    Implement method read.model

--- R/records.R
+++ R/records.R
@@ -68,3 +68,43 @@ setMethod("write", signature=c("records", "character"), definition=function(obje
   write.table(x=code, file=file, row.names=FALSE, col.names=FALSE, quote=FALSE)
 })
 
+#_______________________________________________________________________________
+#----                                read.model                             ----
+#_______________________________________________________________________________
+
+#' Read model file.
+#' 
+#' @param file path to records
+#' @return records object
+#' @export
+read.model <- function(file) {
+  allLines <- read.table(file=file, sep="@")[,1]
+  records <- new("records", list=list())
+  prevRecordIndex <- 1
+  
+  for (index in seq_along(allLines)) {
+    line <- allLines[index]
+    if (isRecordDelimiter(line)) {
+      recordDelimiter <- getRecordDelimiter(line)
+      record <- new(paste0(tolower(recordDelimiter), "_record"), code=character())
+      records@list <- c(records@list, record)
+      if (length(records@list) > 1) {
+        records@list[[length(records@list)-1]]@code <- allLines[(prevRecordIndex + 1):(index-1)]
+      }
+      prevRecordIndex <- index
+    }
+  }
+  # Filling last record
+  records@list[[length(records@list)]]@code <- allLines[(prevRecordIndex + 1):length(allLines)]
+  return(records)
+}
+
+isRecordDelimiter <- function(line) {
+  return(grepl("^s*\\[.*\\]s*$", line))
+}
+
+getRecordDelimiter <- function(line) {
+  return(gsub("\\[(.*)\\]","\\1", line))
+}
+
+
--- tests/testthat/testRecords.R
+++ tests/testthat/testRecords.R
@@ -9,19 +9,25 @@ testFolder <<- "C:/prj/pmxmod/tests/testthat/"
 test_that("Add and get methods", {
   
   records <- new("records",  list=list()) 
-  records <- records %>% addRecord(new("pk_record", code="[PK]"))
-  records <- records %>% addRecord(new("error_record", code="[ERROR]"))
+  records <- records %>% addRecord(new("pk_record", code=""))
+  records <- records %>% addRecord(new("error_record", code=""))
   
   expect_true(records %>% getRecord("PK") %>% length() > 0)
   expect_true(records %>% getRecord("ERROR") %>% length() > 0)
   expect_true(records %>% getRecord("DES") %>% length() == 0)
 })
 
-test_that("Write methods", {
+test_that("Write/Read methods", {
   
   records <- new("records",  list=list()) 
   records <- records %>% addRecord(new("pk_record", code=c("A=1", "B=2")))
   records <- records %>% addRecord(new("error_record", code=c("C=3")))
   
+  # Write model
   records %>% write(file=paste0(testFolder, "write/records/records1.mod"))
+  
+  # Read model
+  records2 <- read.model(file=paste0(testFolder, "write/records/records1.mod"))
+  
+  expect_equal(records, records2)
 })
