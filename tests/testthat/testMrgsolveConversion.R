7bd0442f824f23d7d21fc3164ad76945dd6858ab
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-08-04 15:09:00 +0200

    #6 Expand test in testMrgsolveConversion.R.

--- tests/testthat/testMrgsolveConversion.R
+++ tests/testthat/testMrgsolveConversion.R
@@ -5,6 +5,14 @@ context("Test the conversion to mrgsolve")
 
 test_that("Export method works", {
   model <- getNONMEMModelTemplate(4,4)
+  
+  # Add some comments/line breaks programmatically
+  main <- model@model %>% getByName("MAIN")
+  main@code[1] <- paste(main@code[1], "# EQUATION COMMENT")
+  main@code <- main@code %>% append("")
+  main@code <- main@code %>% append("# THIS IS A COMMENT")
+  model <- model %>% replace(main)
+  
   model <- model %>% add(Bioavailability(compartment=1, rhs="0.75"))
   model <- model %>% add(LagTime(compartment=2, rhs="2"))
   model <- model %>% add(InfusionDuration(compartment=2, rhs="2"))
@@ -33,12 +41,14 @@ test_that("Export method works", {
   
   main <-
     c(
-      "double KA=THETA_KA*exp(ETA_KA);",
+      "double KA=THETA_KA*exp(ETA_KA); // EQUATION COMMENT",
       "double CL=THETA_CL*exp(ETA_CL);",
       "double V2=THETA_V2*exp(ETA_V2);",
       "double V3=THETA_V3*exp(ETA_V3);",
       "double Q=THETA_Q*exp(ETA_Q);",
       "double S2=V2;",
+      "", # Line break
+      "// THIS IS A COMMENT",
       "F_A_DEPOT=0.75;",
       "ALAG_A_CENTRAL=2;",
       "D_A_CENTRAL=2;",
