20b5f3700af26cc5126a75ed22394ead1559a994
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2022-02-01 12:45:00 +0100

    #44 Eliminating some notes of R CMD CHECK.

---
+++ R/global_functions.R
@@ -0,0 +1,20 @@
+
+#'
+#' A empty method only used to declare some of the global function definitions
+#' contained in the package 'methods'.
+#'
+#' @importFrom methods callNextMethod is new show slotNames validObject
+#' @keywords internal
+methodsGlobalFunctions <- function() {
+  
+}
+
+#'
+#' A empty method only used to declare some of the global function definitions
+#' contained in the package 'utils'.
+#'
+#' @importFrom utils hasName read.csv write.csv write.table
+#' @keywords internal
+utilsGlobalFunctions <- function() {
+  
+}
